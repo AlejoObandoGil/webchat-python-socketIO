007f884fec96506db14968f532d004e596a5a320
alejo <alejo.obando.g@gmail.com>
2020-12-04 22:22:10 -0500

    full hd commit

--- app.py
+++ app.py
@@ -3,15 +3,16 @@
 # ------------------------------LIBRERIAS---------------------------------------
 
 # Principales: webApp, BD, cifrado hash, login de flask, websockets
-import os
 from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from passlib.hash import pbkdf2_sha256
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
-from time import strftime, localtime
 
-# secundarias:
+from time import strftime, localtime
+import os
+
+# Secundarias:
 from modelos import *
 from formulario import *
 
@@ -19,13 +20,14 @@ from formulario import *
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET')
+app.secret_key = "SECRET"
 
 # Direccion de BD: postgres de heroku
-# usuario, contraseña, host, puerto, nombre de la BD
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# Usuario, contraseña, host, puerto, nombre de la BD
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://odzwjrzlprudil:8317735ed8c2403e044449e353a227dbc9ca3a0afc17ba794f37cfc40420558d@ec2-54-161-150-170.compute-1.amazonaws.com:5432/d57q5bus76ls43"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# inicializar en BD
+# Inicializar en BD
 db = SQLAlchemy(app)
 #print("\n-conexion a BD habilitada --> heroku-postgres ")
 
@@ -33,17 +35,17 @@ db = SQLAlchemy(app)
 socketio = SocketIO(app)
 #print("-websockets habilidatos \n")
 
-# inicializar controlador de sesion
+# Inicializar controlador de sesion
 login = LoginManager(app)
 login.init_app(app)
 
-# lista donde almacenaremos las salas creadas, por defecto el servidor incia con 1 sola sala
+# Lista donde almacenaremos las salas creadas, por defecto el servidor incia con 1 sola sala
 LISTA_SALAS = ["Principal"]
 
 
 # ----------------------RUTAS DE LOGIN DEL SERVIDOR--------------------------
 
-# ruta principal para index.html
+# Ruta principal para index.html
 @app.route("/", methods=['GET', 'POST'])
 def index():
     # instanciamos desde formulario
@@ -65,7 +67,7 @@ def index():
 # Ruta de inicio de sesion para registro.html
 @app.route("/registro", methods=['GET', 'POST'])
 def registro():
-    # instanciamos desde formulario
+    # Instanciamos desde formulario
     registroForm = Registro()
     # Validamos el formulario que digitamos
     if registroForm.validate_on_submit():
@@ -99,7 +101,7 @@ def registro():
     return render_template("registro.html", form=registroForm)
 
 
-# ruta que recarga el cliente actual conectado
+# Ruta que recarga el cliente actual conectado
 @login.user_loader
 def cargar_usuario(id):
     return User.query.get(int(id))
@@ -118,15 +120,15 @@ def cerrar_sesion():
 def chat():
     nuevaSala = ""
 
-    # metodo POST para validar la creacion de la nueva salida
+    # Metodo POST para validar la creacion de la nueva salida
     if request.method == 'POST':
         nuevaSala = request.form['nueva_sala']
-
         if nuevaSala in LISTA_SALAS:
             flash("¡La sala ya existe, elige otro nombre", "success")
         else:
             LISTA_SALAS.append(nuevaSala)
 
+    # Instanciamos objeto para eliminar sala
     eliminarSala = EliminarSala()
     sala = eliminarSala.input_eliminar_sala.data
     if sala in LISTA_SALAS:
@@ -147,7 +149,7 @@ def chat():
 
     # Siempre redirige a la cuenta del mismo usuario si la sesion esta abierta
     return render_template('chat.html', usuario=current_user.usuario, rooms=LISTA_SALAS, form=eliminarSala, user=user, nUser=nUser)
- # form=eliminarSala
+
 
 
 # ---------------------RUTAS WEBSOCKETS DEL SERVIDOR-------------------------
@@ -180,4 +182,5 @@ def leave(data):
 
 if __name__ == "__main__":
 
-    app.run(debug=True)
+    db.init_app(app)
+    socketio.run(app, debug=True)
